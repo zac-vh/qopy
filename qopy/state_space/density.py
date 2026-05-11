cca50fb3552a9f519e03fed611ab67532b9deb4c
zac-vh <zchrvh@gmail.com>
2026-05-11 18:07:55 +0200

    Update bloch_sphere

--- qopy/state_space/density.py
+++ qopy/state_space/density.py
@@ -9,7 +9,7 @@ def from_ket(ket):
     # Build rho from the vector ket
     ket = np.array(ket)
     if ket.size == 1:
-        rho = np.ones([1, 1])*np.abs(ket)**2
+        rho = np.ones([1, 1]) * np.abs(ket) ** 2
         return rho
     n = len(ket)
     rho = np.zeros([n, n], dtype=complex)
@@ -33,20 +33,20 @@ def random_mixed(N):
         return np.array([[1]])
     u_rand1 = scipy.stats.unitary_group.rvs(N)
     u_rand2 = scipy.stats.unitary_group.rvs(N)
-    rho_diag = np.diag(np.abs(u_rand1[0])**2)
+    rho_diag = np.diag(np.abs(u_rand1[0]) ** 2)
     rho = u_rand2 @ rho_diag @ np.transpose(np.conj(u_rand2))
     return rho
 
 
-def bloch_sphere(theta, phi=0, p=1):
-    ket = np.array([np.cos(theta/2), np.exp(1j*phi)*np.sin(theta/2)])
+def bloch_sphere(theta, phi=0, r=1):
+    ket = np.array([np.cos(theta / 2), np.exp(1j * phi) * np.sin(theta / 2)])
     rho_pure = from_ket(ket)
-    rho_mix = p*rho_pure+(1-p)*np.eye(2)/2
+    rho_mix = r * rho_pure + (1 - r) * np.eye(2) / 2
     return rho_mix
 
 
 def beam_splitter(m, n, eta=0.5):
-    N = m+n+1
+    N = m + n + 1
     eigvals = np.zeros(N)
     for k in range(N):
         eigvals[k] = transition_amplitude(m, n, k, eta) ** 2
@@ -55,19 +55,29 @@ def beam_splitter(m, n, eta=0.5):
 
 def random_unitary(N):
     if N == 1:
-        return np.array([np.exp(1j*random.random()*2*math.pi)])
+        return np.array([np.exp(1j * random.random() * 2 * math.pi)])
     return scipy.stats.unitary_group.rvs(N)
 
 
 def rotate(rho, phi):
-    rotmat = np.diag(np.exp(-1j*phi*np.arange(len(rho))))
+    rotmat = np.diag(np.exp(-1j * phi * np.arange(len(rho))))
     return rotmat @ rho @ np.conj(rotmat)
 
 
 def displacement_matrix(m, n, alpha):
     # return the value of <m|D(alpha)|n>
     dmn = 0
-    for p in range(np.min([m, n])+1):
-        dmn = dmn + np.sqrt(float(math.factorial(m)*math.factorial(n)))*(-1)**(n-p)/(math.factorial(p)*math.factorial(m-p)*math.factorial(n-p))*alpha**(m-p)*np.conj(alpha)**(n-p)
-    dmn = np.exp(-(1/2)*np.abs(alpha)**2)*dmn
+    for p in range(np.min([m, n]) + 1):
+        dmn = dmn + np.sqrt(float(math.factorial(m) * math.factorial(n))) * (-1) ** (
+            n - p
+        ) / (
+            math.factorial(p) * math.factorial(m - p) * math.factorial(n - p)
+        ) * alpha ** (
+            m - p
+        ) * np.conj(
+            alpha
+        ) ** (
+            n - p
+        )
+    dmn = np.exp(-(1 / 2) * np.abs(alpha) ** 2) * dmn
     return dmn
